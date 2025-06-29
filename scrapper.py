39a141656ec6040a9dd3fb8a1fe756e69d986c56
Ankit Khadka <khadka.ankit2004@gmail.com>
2025-06-29 16:54:34 +0545

    Finish Project

---
+++ scrapper.py
@@ -0,0 +1,48 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://books.toscrape.com/"
+
+def scrape_books(url):
+    response = requests.get(url)
+    if response.status_code != 200:
+        print('Failed to load page.')
+        return []
+    response.encoding = response.apparent_encoding
+    soup = BeautifulSoup(response.text,"html.parser")
+    books = soup.find_all("article", class_="product_pod")
+
+    book_list = []
+
+    for book in books:
+        title = book.h3.a['title']
+        
+        price_text = book.find('p', class_='price_color').text 
+
+        currency = price_text[0]
+        price = float(price_text[1:])
+        
+        book_list.append(
+            {
+                "title":title,
+                "currency":currency,
+                "price":price
+            }
+        )
+
+    return book_list
+
+
+all_books = scrape_books(url)
+
+import json
+
+with open("books.json" , 'w' , encoding='utf-8') as f:  # encoding= 'utf-8' and ensure_ascii=False is done so the symbol of the currency comes 
+    books = json.dump(all_books,f , indent=4 , ensure_ascii=False)
+
+import csv
+
+with open("book.csv" , 'w' , newline="" , encoding='utf-8') as f:
+    writer = csv.DictWriter(f , fieldnames=['title','currency','price'])
+    writer.writeheader()
+    writer.writerows(all_books)
